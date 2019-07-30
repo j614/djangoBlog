a303d383d3d5a6cd47fa8a0a5c375871d406ea50
j614 <jashik244@gmail.com>
2019-07-30 17:38:42 +0300

    17:55 30.07

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post,Comment
+
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -11,5 +12,11 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
 
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'update')
+    search_fields = ('name', 'email', 'body')
 
 # Register your models here.
